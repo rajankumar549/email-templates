398ea1930202e8681cfcf79f06fb395654d53bad
Guo Yunhe <guoyunhebrave@gmail.com>
2017-02-16 20:03:21 +0200

    Use inline style and move images out

--- README.md
+++ README.md
@@ -2,7 +2,40 @@
 
 Marketing email templates and static files
 
-1. PureCSS through CDN
-2. Google Fonts CDN
-3. All styles included inside HTML
-4. Images and logos load from https://static.santakani.com/emails
+1. PureCSS framework
+2. Juice to compile CSS inline
+3. Images are hosted at https://static.santakani.com/
+
+Something to remember:
+
+1. SVG images may not be supported. Use PNG instead.
+2. External CSS may not be supported. Use internal or inline style.
+3. External fonts may not be supported. Use images or system fonts.
+4. Margin, positioning and floating may not be supported. Use static layout.
+5. Inline images may not be supported. Always load through remote URL.
+
+Checkout this list for CSS support in popular email clients:
+<https://www.campaignmonitor.com/css/>
+
+## Email clients for testing
+
+Desktop:
+
+- Gmail Web
+- Outlook Web
+- Thunderbird
+- KMail
+- Mail App of macOS
+- Mail App of Windows
+
+Mobile:
+
+- Gmail App
+- Mail App of Android
+- Mail App of iOS
+- K9 Mail
+
+## Spam testing
+
+To avoid email to be marked as spam, test it at <https://www.mail-tester.com/>
+before sending.
--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,20 @@
 var gulp = require('gulp');
-var cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
-var svgmin = require('gulp-svgmin');
+var juice = require('gulp-juice');
 
-gulp.task('minify-css', function() {
-    return gulp.src(['./common/*.css', '!common/*-min.css'])
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(rename({suffix: "-min"}))
-        .pipe(gulp.dest('./common'));
-});
-
-gulp.task('minify-svg', function () {
-    return gulp.src(['./common/*.svg', '!common/*-min.svg'])
-        .pipe(svgmin())
-        .pipe(rename({suffix: "-min"}))
-        .pipe(gulp.dest('./common'));
+gulp.task('compile-html', function () {
+    return gulp.src('./**/*-template.html')
+    .pipe(juice({
+        webResources: {
+            images: false
+        }
+    }))
+    .pipe(rename({suffix: "-inline"}))
+    .pipe(gulp.dest('./'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch('./common/*.css', ['minify-css']);
-    gulp.watch('./common/*.svg', ['minify-svg']);
+    gulp.watch('./**/*-template.html', ['compile-html']);
 });
 
-gulp.task('default', ['minify-css', 'minify-svg']);
+gulp.task('default', ['compile-html']);
